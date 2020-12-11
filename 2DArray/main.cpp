a993864749f733b887a48195e7d56e0162679bb1
unknown <ilonnaa12@gmail.com>
2020-12-11 17:02:07 +0200

    solved task 10

--- 2DArray/main.cpp
+++ 2DArray/main.cpp
@@ -10,6 +10,8 @@ void fillMatrix(int** matrix, int n, int m);
 void deleteMatrix(int** matrix, int n, int m);
 void changeRowColumn(int** matrix, int n, int m, int k); //task11
 
+int getRowNumWithMaxAvg(int** matrix, int n, int m);
+
 int main() {
 	srand(time(NULL));
 	int size1 = 5;
@@ -22,6 +24,16 @@ int main() {
 	cout << "Matrix after chaner " << Index << "row and column" << endl;
 	printMatrix(matrix1, size1, size1);
 	deleteMatrix(matrix1, size1, size1);
+
+	// task 10
+	int size2 = 3;
+	int** matrix2 = formMatrix(size2, size2);
+	fillMatrix(matrix2, size2, size2);
+	cout << "Matrix: " << endl;
+	printMatrix(matrix2, size2, size2);
+	int index1 = getRowNumWithMaxAvg(matrix2, size2, size2);
+	cout << "Row index with max average: " << index1 + 1 << endl;
+
 
 	return 0;
 }
@@ -79,3 +91,26 @@ void changeRowColumn(int** matrix, int n, int m, int k)
 	}
 	*/
 }
+
+
+	int getRowNumWithMaxAvg(int** matrix, int n, int m)
+	{
+		
+		int rowIndex = 0;
+		double max = -9999;
+		for (int i = 0; i < n; i++) {
+			int sum = 0;
+			for (int j = 0; j < m; j++) {
+				sum += matrix[i][j];
+			}
+			double avg = sum / m; // avg
+			if (avg > max) {
+				max = avg;
+				rowIndex = i;
+			}
+		}
+		return rowIndex;
+	}
+
+	
+
